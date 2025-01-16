1d788050483da7de1a6bbfe9cd5b5f6de8479a28
RafaAlbarran <rafa.albarran@gmail.com>
2025-01-16 13:16:21 +0100

    Commit incial

---
+++ JS/webBaile/script.js
@@ -0,0 +1,7 @@
+
+
+
+document.querySelector('#inicio').style.display=none;
+document.querySelector('#cursos').style.display=none;
+document.querySelector('#escuela').style.display=none;
+document.querySelector('#contacto').style.display=none;
